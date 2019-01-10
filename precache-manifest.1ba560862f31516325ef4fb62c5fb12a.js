f29a100da22c731a1e275f7ea2bb157d74f4d694
Ilya <tishonok.ilya@ya.ru>
2019-01-10 15:48:02 +0300

    Updates

---
+++ precache-manifest.1ba560862f31516325ef4fb62c5fb12a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "63ab4e76f67725309533",
+    "url": "/homework_10/static/js/runtime~main.63ab4e76.js"
+  },
+  {
+    "revision": "9540c344efb6c582875b",
+    "url": "/homework_10/static/js/main.9540c344.chunk.js"
+  },
+  {
+    "revision": "2905ae5f85a46f245e28",
+    "url": "/homework_10/static/js/1.2905ae5f.chunk.js"
+  },
+  {
+    "revision": "b506909be47517cff3cb86d95d12b078",
+    "url": "/homework_10/index.html"
+  }
+];
